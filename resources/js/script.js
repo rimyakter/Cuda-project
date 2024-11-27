c570bc2ca57ee2b89976841cbe24c15ecdd4bf5d
Rimy Akter <rimy@gmail.com>
2024-11-27 21:40:35 +0600

    Added cuda project to remote

---
+++ resources/js/script.js
@@ -0,0 +1,10 @@
+//mixitUp
+var mixer = mixitup('.grid-portfolio-container');
+
+//Hamburger Menu
+function openMenu(){
+    document.getElementById("menu-bar") .style .height = "100vh";
+}
+function closeMenu(){
+    document.getElementById("menu-bar") .style .height = "0%";
+}
